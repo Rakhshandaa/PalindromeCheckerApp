7fbd5835831638780acf59a161448ea28a2bd8b6
your GitHub username <your GitHub email>
2026-02-25 09:10:54 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,14 @@
-public class PalindromeCheckerApp {
-}
+
+
+     public static void main(String[] args) {
+
+        System.out.println("==========================================");
+        System.out.println("     WELCOME TO PALINDROME CHECKER APP");
+        System.out.println("==========================================");
+        System.out.println("Application Version: 1.0");
+        System.out.println("------------------------------------------");
+        System.out.println("This application will check whether");
+        System.out.println("a given string is a palindrome.");
+        System.out.println("==========================================");
+
+    }
